edbcf61fbcd1a5752128085d2a7a3f720237ad42
Bhremada Fevreano <bhremada.fka@gmail.com>
2022-04-23 13:54:00 +0000

    solve challenge

--- server.js
+++ server.js
@@ -24,7 +24,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let { date } = req.params
 
+  if (!date) {
+    const currentDate = new Date()
+    return res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toGMTString()
+    })
+  }
+
+  // if date is in number format, change type to number
+  if (!isNaN(date)) {
+    date = Number(date)
+  }
+
+  const parsedDate = new Date(date)
+
+  if (parsedDate == 'Invalid Date') {
+    return res.json({
+      error: 'Invalid Date'
+    })
+  }
+
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toGMTString()
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
